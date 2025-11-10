34f20aceffef6af78606501d14fe75287245a519
frmeister <frmeister1337@gmail.com>
2025-11-10 19:06:28 +0500

    Add project files.

---
+++ Tyuiu.MolchanovIV.Sprint4.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MolchanovIV.Sprint4.Task0.V23.Lib;
+
+namespace Tyuiu.MolchanovIV.Sprint4.Task0.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int[] array = new int[] { 9, 3, 7, 1, 5, 5, 3, 2, 1, 7 };
+            var res = ds.GetMultOddArrEl(array);
+
+            Assert.AreEqual(res, 99225);
+
+        }
+    }
+}
---
+++ Tyuiu.MolchanovIV.Sprint4.Task0.V23/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MolchanovIV.Sprint4.Task0.V23.Lib;
+
+namespace Tyuiu.MolchanovIV.Sprint4.Task0.V23
+{
+    class Programm
+    {
+        public static void main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int[] arr = new int[] { 9, 3, 7, 1, 5, 5, 3, 2, 1, 7 };
+
+            Console.Title = "Спринт #4 | Выполнил: Молчанов И. В. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Молчанов Иван Владимирович | РППБ-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать произведение   *");
+            Console.WriteLine("* нечетных элементов массива. {9 ,3 ,7 ,1 ,5 ,5 ,3 ,2 ,1 ,7}              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Элементы массива: { ");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.GetMultOddArrEl(arr));
+
+        }
+    }
+}
