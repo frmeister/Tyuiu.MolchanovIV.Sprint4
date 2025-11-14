e22f4b7e1039764c09b262317d97ab49643a8c5d
frmeister <frmeister1337@gmail.com>
2025-11-14 20:10:33 +0500

    Task2

---
+++ Tyuiu.MolchanovIV.Sprint4.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.MolchanovIV.Sprint4.Task2.V9.Lib;
+
+namespace Tyuiu.MolchanovIV.Sprint4.Task2.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+
+            var altRes = 1;
+
+            int[] arr = new int[14];
+
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                arr[i] = rnd.Next(1, 7); if (arr[i] % 2 == 0) altRes *= arr[i];
+            }
+
+            var res = ds.Calculate(arr);
+
+            Assert.AreEqual(altRes, res);
+            
+        }
+    }
+}
---
+++ Tyuiu.MolchanovIV.Sprint4.Task2.V9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MolchanovIV.Sprint4.Task2.V9.Lib;
+
+namespace Tyuiu.MolchanovIV.Sprint4.Task2.V9
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+            int[] arr = new int[14];
+
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                arr[i] = rnd.Next(1, 7);
+            }
+
+            Console.Title = "Спринт #4 | Выполнил: Молчанов И. В. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Молчанов Иван Владимирович | РППБ-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 14 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 1 до 7 подсчитать произведение                *");
+            Console.WriteLine("* четных элементов массива.                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Элементы массива: { ");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine(" }");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(arr));
+
+        }
+    }
+}
